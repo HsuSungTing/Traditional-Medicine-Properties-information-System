5d108e4fa4a6754adbce007c0300a5f07f2528a3
lilyowo <d10630818@gapps.fg.tp.edu.tw>
2023-07-21 16:22:38 +0800

    advanced search page skin 90% complete version

--- client/search_page/search.js
+++ client/search_page/search.js
@@ -28,6 +28,8 @@ function opneORclose(){
 // document.getElementById("med_name").setAttribute("href","../home_page/index.html");
 // document.getElementById("med_info").setAttribute("href",`../info_mid_page/index2.html?number=${nameid}`);
 // document.getElementById("med_ref").setAttribute("href", `../info_mid_page/index2.html?number=${nameid}`);
+
+////////三個條件的按鈕事件處理///////
 const Ex_cardName = document.getElementById('Ex_cardName');
 const Cl_cardName = document.getElementById('Cl_cardName');
 const Ch_cardName = document.getElementById('Ch_cardName');
@@ -53,7 +55,6 @@ for (let i = 0; i < cardNames.length; i++) {
     });
 }
 
-
 function DownExpand(expandId) {
     if(expandId===0){
         content = document.getElementById("Ex_subCardWrapper");
@@ -72,12 +73,16 @@ function DownExpand(expandId) {
         btn.style.color = 'black';
         downPage[expandId]=false;
     } else {
-        content.style.display = 'block'; // 显示内容
+        content.style.display = 'flex'; // 显示内容
         btn.style.backgroundColor='gray';
         btn.style.color='white';
         downPage[expandId]=true;
     }    
 }
+////////三個條件的按鈕事件處理///////
+
+
+/////////處理條件屬於數值輸入的狀況///////
 function adjustInputWidth(input) {
     const valueLength = input.value.length;
     const minWidth = 30; // 设置输入框的最小宽度
@@ -88,6 +93,79 @@ function adjustInputWidth(input) {
     const newWidth = Math.min(Math.max(valueLength * charWidth, minWidth), maxWidth);
     input.style.width = newWidth + "px";
 }
-Ex_subCardName = ['溶劑','其他'];
-Cl_subCardName = ['廠牌', '型號', '長度', '寬度', '粒徑', '溫度'];
+
+function createValueItem(containerID) {
+    const container = document.getElementById(containerID);
+    // 创建<div class="ValueItem">元素
+    const valueItem = document.createElement("div");
+    valueItem.classList.add("ValueItem");
+    valueItem.innerHTML = '：<input type="number" class="numberInput" oninput="adjustInputWidth(this)"> &le; x &le; <input type="number" class="numberInput" oninput="adjustInputWidth(this)">';
+    container.appendChild(valueItem);
+}
+
+
+
+/////////處理條件屬於數值輸入的狀況///////
+
+
+//////Create subCard///////////
+Ex_subCardName = [['溶劑',['乙醇','甲醇']],['條件2',['選項1','選項2']]];
+Cl_subCardName = [['廠牌',[1]], ['型號',[1]], ['長度',[1]], ['寬度',[1]], ['粒徑',[1]], ['溫度',[1]]];
 Ch_subCardName = ['Mobile phase A', 'Mobile phase B', 'Detection wavelength', 'Injection'];
+
+
+
+function createSubCard(containerID, Names){
+    const container = document.getElementById(containerID);
+    Names.forEach(function(element){
+        const subCard = document.createElement('div');
+        
+        const cardName = document.createElement('div');
+        cardName.classList.add('cardName');
+        cardName.textContent = element[0];
+
+        const ItemsWrapper = document.createElement('div');
+        ItemsWrapper.classList.add('ChoiceItemsWrapper');
+        ItemsWrapper.id = element[0]+"_ItemsWrapper";
+        
+        element[1].forEach(function(ele){
+            if (typeof ele === 'number') {//創一個輸入框，並且這個itemNames的array項目只有一個
+                // createValueItem(ItemsWrapper.id)
+                const valueItem = document.createElement("div");
+                valueItem.classList.add("ValueItem");
+                valueItem.innerHTML = '：<input type="number" class="numberInput" oninput="adjustInputWidth(this)"> &le; x &le; <input type="number" class="numberInput" oninput="adjustInputWidth(this)">';
+                ItemsWrapper.appendChild(valueItem);
+            } else {//對多個名稱選項創ChoiceItem
+                const Item = document.createElement('div');
+                Item.classList.add('ChoiceItem');
+                Item.textContent = ele;
+                ItemsWrapper.appendChild(Item);
+            }
+        });
+
+
+        subCard.appendChild(cardName);
+        subCard.appendChild(ItemsWrapper);
+        
+        container.appendChild(subCard);
+
+        
+    });
+}
+createSubCard('Ex_subCardWrapper',Ex_subCardName);
+createSubCard('Cl_subCardWrapper',Cl_subCardName);
+//createSubCard('Ch_subCardWrapper',Ch_subCardName);
+
+// function createItems(containerID, itemNames){
+//     const container = document.getElementById(containerID);
+//     itemNames.forEach(function(element){
+//         if (typeof element === 'number') {//創一個輸入框，並且這個itemNames的array項目只有一個
+//             createValueItem(containerID)
+//         } else {//對多個名稱選項創ChoiceItem
+//             Item = document.createElement('div');
+//             Item.classList.add('ChoiceItem');
+//             Item.textContent = element;
+//             container.appendChild(Item);
+//         }
+//     });
+// }
